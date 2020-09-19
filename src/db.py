5c84cb27277f2ef6f16f415b7953c3c3c8f40781
dannykspaw <dannykspaw@gmail.com>
2020-09-19 12:03:42 -0500

    pushing connection to db

--- src/db.py
+++ src/db.py
@@ -1,19 +1,22 @@
 # handles connections and initialization of the db
 
 from os import getenv
+import psycopg2
 
-'''
-
-export DB_HOST=localhost
-
-'''
 
 def connect():
     print('connecting to the db')
-    # connect to the db
+    connect = psycopg2.connect(
+    host = getenv('DB_HOST'),
+    database = getenv('DB_NAME'),
+    user = getenv('DB_USER'),
+    password = getenv('DB_PASSWORD')
+    )
     print('host', getenv('DB_HOST'))
-
-    # create tables if they don't exist
-
+    print('database', getenv('DB_NAME'))
+    print('user', getenv('DB_USER'))
+    print('password', getenv('DB_PASSWORD'))
+    # create tables if they don't
     # return the database cursor
-    return None
+    print(connect.cursor)
+connect()
