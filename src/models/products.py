05857c7b6a376603099eda0666e4b45885b879b4
Terran Blake <terranblake@gmail.com>
2020-10-10 15:17:57 -0500

    use models file for hookedonhallmark and ornament_shop integrations

--- src/models/products.py
+++ src/models/products.py
@@ -1,5 +1,16 @@
-columns = ['id', 'vendor', 'sku', 'name', 'price', 'brand',
-           'availability', 'release_year', 'vendor_id', 'link', 'last_synced_at', 'created_at']
+columns = [
+    'id',
+    'vendor',
+    'sku',
+    'name',
+    'price',
+    'brand',
+    'availability',
+    'release_year',
+    'link',
+    'last_synced_at',
+    'created_at'
+]
 
 # todo: create base model class/interface that takes a mapping of columns
 #       and types and creates the tables in the database, as well as providing
